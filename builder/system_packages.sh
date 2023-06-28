0c87cea7637bd87b845ad2ccb19984707ebd6f99
Justin Merrell <merrelljustin@gmail.com>
2023-06-28 17:24:59 -0400

    fix: docker file and handler

--- builder/system_packages.sh
+++ builder/system_packages.sh
@@ -3,13 +3,13 @@
 apt-get install ffmpeg libsm6 libxext6  -y
 
 apt-get update && \
-    apt-get upgrade -y && \
-    apt-get install -y software-properties-common \
-    add-apt-repository ppa:deadsnakes/ppa && \
-    apt-get update && \
-    apt-get install -y --no-install-recommends \
-    ffmpeg libsm6 libxext6 git curl libgl1 libglib2.0-0 libgoogle-perftools-dev && \
-    python3.10-dev python3.10-tk python3-html5lib python3-apt python3-pip python3.10-distutils && \
-    apt-get autoremove -y && \
-    apt-get clean -y && \
-    rm -rf /var/lib/apt/lists/*
+apt-get upgrade -y && \
+apt-get install -y software-properties-common && \
+add-apt-repository ppa:deadsnakes/ppa && \
+apt-get update && \
+apt-get install -y --no-install-recommends \
+ffmpeg libsm6 libxext6 git curl libgl1 libglib2.0-0 libgoogle-perftools-dev && \
+python3.10-dev python3.10-tk python3-html5lib python3-apt python3-pip python3.10-distutils && \
+apt-get autoremove -y && \
+apt-get clean -y && \
+rm -rf /var/lib/apt/lists/*
